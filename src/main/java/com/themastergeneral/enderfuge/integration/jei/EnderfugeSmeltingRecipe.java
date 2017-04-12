9edabe16be1f9f6ac1ca7a6bf6a97bc225710769
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-04-12 17:30:30 -0400

    JEI integration!

---
+++ src/main/java/com/themastergeneral/enderfuge/integration/jei/EnderfugeSmeltingRecipe.java
@@ -0,0 +1,49 @@
+package com.themastergeneral.enderfuge.integration.jei;
+
+import java.awt.Color;
+import java.util.Collections;
+import java.util.List;
+
+import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.BlankRecipeWrapper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.item.ItemStack;
+
+import com.themastergeneral.enderfuge.common.processing.EnderfugeRecipes;
+
+public class EnderfugeSmeltingRecipe extends BlankRecipeWrapper  {
+	private final List<List<ItemStack>> inputs;
+	private final ItemStack output;
+
+	public EnderfugeSmeltingRecipe(List<ItemStack> inputs, ItemStack output) {
+		this.inputs = Collections.singletonList(inputs);
+		this.output = output;
+	}
+
+	@Override
+	public void getIngredients(IIngredients ingredients) {
+		ingredients.setInputLists(ItemStack.class, inputs);
+		ingredients.setOutput(ItemStack.class, output);
+	}
+
+	public List<List<ItemStack>> getInputs() {
+		return inputs;
+	}
+
+	public List<ItemStack> getOutputs() {
+		return Collections.singletonList(output);
+	}
+
+	@Override
+	public void drawInfo(Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
+		EnderfugeRecipes furnaceRecipes = EnderfugeRecipes.instance();
+		float experience = furnaceRecipes.getSmeltingExperience(output);
+		if (experience > 0) {
+			String experienceString = "gui.jei.category.smelting.experience", experience1;
+			FontRenderer fontRendererObj = minecraft.fontRendererObj;
+			int stringWidth = fontRendererObj.getStringWidth(experienceString);
+			fontRendererObj.drawString(experienceString, recipeWidth - stringWidth, 0, Color.gray.getRGB());
+		}
+	}
+}

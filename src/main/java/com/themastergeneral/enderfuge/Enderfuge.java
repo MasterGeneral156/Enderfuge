b64cc1bb3c28f5e647d7059a0ba715089922d8ab
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-06-20 13:43:01 -0400

    Re-re-re-re-re-re-re-readded JSON recipes

--- src/main/java/com/themastergeneral/enderfuge/Enderfuge.java
+++ src/main/java/com/themastergeneral/enderfuge/Enderfuge.java
@@ -19,7 +19,7 @@ public class Enderfuge
 {
 	public static final String MODID = "enderfuge";
     public static final String MODNAME = "Enderfuge";
-    public static final String VERSION = "1.1.5";
+    public static final String VERSION = "1.1.6";
     public static final String acceptedMinecraftVersions = "1.12";
     public static final String DEPENDENCIES = "required-after:ctdcore@[1.1.2,];";
 	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/Enderfuge.json";

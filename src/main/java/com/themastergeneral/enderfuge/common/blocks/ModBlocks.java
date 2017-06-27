4914286bf3335556e01daa62cfb72f03f9b490a8
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-06-26 20:15:19 -0400

    Have blocks be registered through CTD Core

--- src/main/java/com/themastergeneral/enderfuge/common/blocks/ModBlocks.java
+++ src/main/java/com/themastergeneral/enderfuge/common/blocks/ModBlocks.java
@@ -1,12 +1,13 @@
 package com.themastergeneral.enderfuge.common.blocks;
 
+import com.themastergeneral.ctdcore.block.RegisterBlock;
 import com.themastergeneral.enderfuge.client.renders.ItemModelProvider;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-public class ModBlocks 
+public class ModBlocks extends RegisterBlock
 {
 	public static BlockEnderfuge enderfuge = new BlockEnderfuge("enderfuge", false);
 	public static BlockEnderfuge enderfuge_active = new BlockEnderfuge("enderfuge_active", true);
@@ -15,23 +16,4 @@ public class ModBlocks
 		enderfuge = register(new BlockEnderfuge("enderfuge", false));
 		enderfuge_active = register(new BlockEnderfuge("enderfuge_active", true));
 	}
-	public static <T extends Block> T register (T block, ItemBlock itemBlock) 
-	 {
-		 GameRegistry.register(block);
-		 if(itemBlock != null) 
-		 {
-			 GameRegistry.register(itemBlock);
-		 }
-		 if(block instanceof ItemModelProvider) 
-		 {
-			 ((ItemModelProvider)block).registerItemModel(itemBlock);
-		 }
-		 return block;
-	 }
-	public static <T extends Block> T register(T block) 
-	 {
-		 ItemBlock itemBlock = new ItemBlock(block);
-		 itemBlock.setRegistryName(block.getRegistryName());
-		 return register(block, itemBlock);
-	 }
 }

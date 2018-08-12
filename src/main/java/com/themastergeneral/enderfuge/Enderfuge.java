319095c310ed71d34666232f42371470b2f9146d
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2018-08-11 20:02:37 -0400

    Forgot to increment build number

--- src/main/java/com/themastergeneral/enderfuge/Enderfuge.java
+++ src/main/java/com/themastergeneral/enderfuge/Enderfuge.java
@@ -22,11 +22,11 @@ import com.themastergeneral.enderfuge.utils.IMCHandler;
 public class Enderfuge {
 	public static final String MODID = "enderfuge";
 	public static final String MODNAME = "Enderfuge";
-	public static final String VERSION = "1.4.6";
+	public static final String VERSION = "1.4.7";
 	public static final String acceptedMinecraftVersions = "1.12.2";
 	public static final String DEPENDENCIES = "required-after:ctdcore@[1.3.3,];";
 	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/Enderfuge.json";
-	public static final String Fingerprint = "5101015479fe39f20b47f365472250d312a50a57";
+	public static final String Fingerprint = "b50fe22651b9d97d9d6631514a6a57cd6174b0dc";
 
 	@Instance
 	public static Enderfuge instance = new Enderfuge();

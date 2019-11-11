529efbb2fad7734570907ade5cdbe399c09e8f5f
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2019-11-10 20:52:00 -0500

    Update deps

--- src/main/java/com/themastergeneral/enderfuge/Enderfuge.java
+++ src/main/java/com/themastergeneral/enderfuge/Enderfuge.java
@@ -25,7 +25,7 @@ public class Enderfuge {
 	public static final String MODNAME = "Enderfuge";
 	public static final String VERSION = "1.5.0";
 	public static final String acceptedMinecraftVersions = "1.12.2";
-	public static final String DEPENDENCIES = "required-after:ctdcore@[1.4.0,];after:ic2;after:thermalfoundation;after:crafttweaker;";
+	public static final String DEPENDENCIES = "required-after:ctdcore@[1.4.2,];after:ic2;after:thermalfoundation;after:crafttweaker;";
 	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/Enderfuge.json";
 	public static final String Fingerprint = "b50fe22651b9d97d9d6631514a6a57cd6174b0dc";
 

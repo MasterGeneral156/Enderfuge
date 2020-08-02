5af54f690c5482e3f29d46f86503451adf261c00
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-08-02 00:02:49 -0400

    Finished CT support.

---
+++ src/main/java/com/themastergeneral/enderfuge/integration/ct/Enderfuge.java
@@ -0,0 +1,42 @@
+package com.themastergeneral.enderfuge.integration.ct;
+
+import javax.annotation.Nullable;
+
+import com.themastergeneral.enderfuge.common.processing.EnderfugeFuel;
+import com.themastergeneral.enderfuge.common.processing.EnderfugeRecipes;
+
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+@ZenClass("mods.enderfuge")
+@ZenRegister
+public class Enderfuge 
+{
+	@ZenMethod
+    public static void addRecipe(IItemStack input, IItemStack output, @Nullable float xp) 
+	{
+		EnderfugeRecipes.instance().addSmeltingRecipe(CraftTweakerMC.getItemStack(input), CraftTweakerMC.getItemStack(output), xp);
+	}
+	
+	@ZenMethod
+    public static void removeRecipe(IItemStack input, IItemStack output) 
+	{
+		EnderfugeRecipes.instance().removeSmelting(CraftTweakerMC.getItemStack(input), CraftTweakerMC.getItemStack(output));
+	}
+	
+	@ZenMethod
+    public static void addFuel(IItemStack input, int ticks) 
+	{
+		EnderfugeFuel.instance().addEnderfugeFuel(CraftTweakerMC.getItemStack(input), ticks);
+	}
+	
+	@ZenMethod
+    public static void removeFuel(IItemStack input) 
+	{
+		EnderfugeFuel.instance().removeFuel(CraftTweakerMC.getItemStack(input));
+	}
+	
+}
